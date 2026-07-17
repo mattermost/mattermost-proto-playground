5173a1b8a5312705040e3ddffa468d9610a40fb3
Matthew Birtch <2040554+matthewbirtch@users.noreply.github.com>
2026-07-17 15:26:40 -0400

    fixes to recording pill

--- src/guidelines/components/recording-pill/recording-pill.specimen.tsx
+++ src/guidelines/components/recording-pill/recording-pill.specimen.tsx
@@ -4,7 +4,9 @@ import styles from '@/styles/library-demo/components.module.scss';
 export default function RecordingPillLibrary() {
   return (
     <>
-      <div className={styles['components__button-block']}>
+      <div
+        className={`${styles['components__button-block']} ${styles['components__button-block--calls-bg']}`}
+      >
         <div className={styles['components__button-row']}>
           <span className={styles['components__instance-label']}>States</span>
           <RecordingPill state="Initializing" />
